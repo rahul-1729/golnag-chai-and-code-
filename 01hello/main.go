bb8f390e28427dc7315c3ff037b91cc0eda08425
rahul-1729 <rk9763981@gmail.com>
2024-08-17 22:56:53 +0530

    int type

--- 01hello/main.go
+++ 01hello/main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello from LearnCodeonline.in")
-	fmt.Println("hello from LearnCodeonline.in")
+
 }
---
+++ 02variables/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Rahul"
+	fmt.Println(username)
+	fmt.Printf("Variable ids of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable ids of type: %T \n", isLoggedIn)
+
+	var smallval int = 256
+	fmt.Println(smallval)
+	fmt.Printf("Variable ids of type: %T \n", smallval)
+
+	var smallfloat float64 = 255.88888888888888
+	fmt.Println(smallfloat)
+	fmt.Printf("Variable ids of type: %T \n", smallfloat)
+
+	var anotherVar int
+	fmt.Println(anotherVar)
+	fmt.Printf("variable is of type: %T \n", anotherVar)
+}
