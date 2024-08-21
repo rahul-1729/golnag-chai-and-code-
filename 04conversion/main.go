29b476a66089c9575bb9ba3fee098c3ee5a89f73
rahul-1729 <rk9763981@gmail.com>
2024-08-21 22:48:31 +0530

    no changes

--- 04conversion/main.go
+++ 04conversion/main.go
@@ -13,9 +13,6 @@ func main() {
 	fmt.Println("Welcom to our Pizza")
 	fmt.Println("Please rate our Pizza between 1 and 5")
 
-	fmt.Println("Welcom to our Pizza")
-	fmt.Println("Please rate our Pizza between 1 and 5")
-
 	reader := bufio.NewReader(os.Stdin)
 
 	input, _ := reader.ReadString('\n')
